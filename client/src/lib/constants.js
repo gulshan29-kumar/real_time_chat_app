1e7a04843bee0e687d562734bca1e9e8e4fde350
Gulshan Kumar <gulshankumar29082006@gmail.com>
2026-08-29 12:24:00 +0530

    feat(client): create constants module for chat event names and endpoints

---
+++ client/src/lib/constants.js
@@ -0,0 +1,17 @@
+/**
+ * Application Constants
+ * Author: Gulshan Kumar (IIIT Ranchi)
+ */
+
+export const SOCKET_EVENTS = {
+  CONNECT: 'connection',
+  DISCONNECT: 'disconnect',
+  NEW_MESSAGE: 'newMessage',
+  GET_ONLINE_USERS: 'getOnlineUsers'
+};
+
+export const APP_CONFIG = {
+  MAX_IMAGE_SIZE_MB: 4,
+  DEFAULT_AVATAR: '/assets/avatar_icon.png',
+  APP_NAME: 'QuickChat'
+};
