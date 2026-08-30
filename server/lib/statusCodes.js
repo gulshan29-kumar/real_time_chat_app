1acad82bd01d2e43c09d89a54713099661555c43
Gulshan Kumar <gulshankumar29082006@gmail.com>
2026-08-30 16:52:00 +0530

    feat(server): add centralized HTTP status codes constant file

---
+++ server/lib/statusCodes.js
@@ -0,0 +1,12 @@
+/**
+ * Standard HTTP Status Codes
+ */
+export const HTTP_STATUS = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_ERROR: 500
+};
