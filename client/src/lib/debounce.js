9864c221b209cfa354ea51039294fb93dea7fb7e
Gulshan Kumar <gulshankumar29082006@gmail.com>
2026-09-03 17:19:00 +0530

    feat(client): add user search debouncing helper utility

---
+++ client/src/lib/debounce.js
@@ -0,0 +1,10 @@
+/**
+ * Debounce helper for search queries
+ */
+export const debounce = (func, delay = 300) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+};
