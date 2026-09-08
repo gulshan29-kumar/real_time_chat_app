ca6ffef8900d2857dd13909034de07674384a295
Gulshan Kumar <gulshankumar29082006@gmail.com>
2026-09-08 19:53:00 +0530

    feat(server): add request execution time tracking middleware

---
+++ server/middleware/requestTimer.js
@@ -0,0 +1,11 @@
+/**
+ * Express middleware to log request latency
+ */
+export const requestTimer = (req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    console.log(`[HTTP] ${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`);
+  });
+  next();
+};
